058d2495ae2d2ab64f253718eafc8950eefa868c
Mohammadian <ryanm64@tamu.edu>
2026-02-14 16:44:19 -0600

    lab4 draft

---
+++ labs/lab04/lab4.py
@@ -0,0 +1,33 @@
+import arcpy
+
+arcpy.env.workspace = r'\\storage.it.tamu.edu\TAMU\OAL\Homes\ryanm64\GEOG Labs\codes_env'
+folder_path = r'\\storage.it.tamu.edu\TAMU\OAL\Homes\ryanm64\GEOG Labs'
+gdb_name = 'Test.gdb'
+gdb_path = folder_path + '\\' + gdb_name
+arcpy.CreateFileGDB_management(folder_path, gdb_name)
+
+csv_path = r'C:\Users\local_ryanm64\Documents\Mohammadian-online-GEOG676-spring2025\labs\lab04\garages.csv'
+garage_layer_name = 'Garage_Points'
+garages = arcpy.MakeXYEventLayer_management(csv_path, 'X', 'Y', garage_layer_name)
+
+input_layer = garages
+arcpy.FeatureClassToGeodatabase_conversion(input_layer, gdb_path)
+garage_points = gdb_path + '\\' + garage_layer_name
+
+campus = r'C:\Users\local_ryanm64\Documents\Mohammadian-online-GEOG676-spring2025\labs\lab04\Campus.gdb'
+buildings_campus = campus + '\Structures'
+buildings = gdb_path + '\\' + 'Buildings'
+
+arcpy.Copy_management(buildings_campus, buildings)
+
+spatial_ref = arcpy.Describe(buildings).spatialReference
+arcpy.Project_management(garage_points, gdb_path + '\Garage_Points_reprojected', spatial_ref)
+
+distance = input("Enter buffer distance in meters: ")
+distance_m = distance + " Meters"
+
+garageBuffered = arcpy.Buffer_analysis(gdb_path + '\Garage_Points_reprojected', gdb_path + '\Garage_Points_buffered', distance_m)
+
+arcpy.Intersect_analysis([garageBuffered, buildings], gdb_path + '\Garage_Building_Intersection', 'ALL')
+
+arcpy.TableToTable_conversion(gdb_path + '\Garage_Building_Intersection', '\\storage.it.tamu.edu\TAMU\OAL\Homes\ryanm64\GEOG Labs', 'nearbyBuildings.csv')
